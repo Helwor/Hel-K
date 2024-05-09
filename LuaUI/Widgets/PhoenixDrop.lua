e301abf653c3549d0c0296b87cdbb535f8ca45bb
Helwor <helwor@yahoo.com>
2024-05-09 19:41:03 +0200

    some cleaning

--- LuaUI/Widgets/PhoenixDrop.lua
+++ LuaUI/Widgets/PhoenixDrop.lua
@@ -15,54 +15,49 @@ end
 local Echo = Spring.Echo
 
 local spGetUnitPosition = Spring.GetUnitPosition
-local spValidUnitID = Spring.ValidUnitID
-local spGetUnitIsDead = Spring.GetUnitIsDead
 local spGetGroundHeight = Spring.GetGroundHeight
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
-local spGiveOrder = Spring.GiveOrder
 local spGetUnitRulesParam = Spring.GetUnitRulesParam
 local spGetSelectedUnitsSorted = Spring.GetSelectedUnitsSorted
--- local spGetMyTeamID = Spring.GetMyTeamID
 local spGetUnitHeading = Spring.GetUnitHeading
--- local spGetUnitWeaponState = Spring.GetUnitWeaponState
 local spGetUnitRulesParam = Spring.GetUnitRulesParam
 local spGetUnitVelocity = Spring.GetUnitVelocity
+-- local spValidUnitID = Spring.ValidUnitID
+-- local spGetUnitIsDead = Spring.GetUnitIsDead
+-- local spGetUnitWeaponState = Spring.GetUnitWeaponState
 -- local spGetUnitIsStunned = Spring.GetUnitIsStunned
 
 local CMD_INSERT, CMD_OPT_ALT, CMD_OPT_SHIFT, CMD_OPT_INTERNAL = CMD.INSERT, CMD.OPT_ALT, CMD.OPT_SHIFT, CMD.OPT_INTERNAL
-local CMD_MOVE, CMD_ATTACK, CMD_REMOVE = CMD.MOVE, CMD.ATTACK, CMD.REMOVE
+local CMD_ATTACK, CMD_REMOVE = CMD.ATTACK, CMD.REMOVE
 
 
 include('keysym.h.lua')
 local DROP_KEY = KEYSYMS.D
 KEYSYMS = nil
 
+-------------
 local dev = false
+local f, Debug
+if dev then
+    f =  VFS.Include('LuaUI\\Widgets\\UtilsFunc.lua')
+    option_path = 'Hel-K/'  .. widget:GetInfo().name
+    Debug = { -- default values
+    active = false, -- no debug, no hotkey active without this
+    global = false, -- global is for no key : 'Debug(str)'
 
-local f = dev and VFS.Include('LuaUI\\Widgets\\UtilsFunc.lua')
-
-------------- DEBUG CONFIG
-local Debug = { -- default values
-    active=false, -- no debug, no hotkey active without this
-    global=false -- global is for no key : 'Debug(str)'
-
-}
+    }
+end
+-------------
 
 
 local phoenixDefID = UnitDefNames['bomberriot'].id
-
 local selectedPhoenixes = false
-
--------------
-
 
 -- CONFIG -- 
 -- local PING_LEEWAY = 0.02
 local opt = {
     removeAnyAttack = true
 }
-
-
 --
 
 
@@ -72,7 +67,6 @@ local function IsReloaded(id)
 end
 
 local function GetClosestDropLocation(id)
-    -- Echo("spGetUnitHeading(id), spGetUnitPosition(id) is ", spGetUnitHeading(id), spGetUnitPosition(id))
     local bx ,by ,bz = spGetUnitPosition(id)
     if not bx then
         return
@@ -81,9 +75,9 @@ local function GetClosestDropLocation(id)
     if not vx then
         return
     end
-    if vx < 5 and vz < 5 then -- in case the unit is temporarily slowed down and stunned
-        vx, vz = vx*2, vz * 2
-    end 
+    -- if vx < 5 and vz < 5 then -- in case the unit is temporarily slowed down and stunned
+    --     vx, vz = vx * 2, vz * 2
+    -- end 
     local gx, gz = bx + vx * 20, bz + vz * 20
     local gy = spGetGroundHeight(gx, gz)
     return gx, gy, gz
@@ -92,10 +86,12 @@ end
 local function InsertAttackGround(id, x,y,z)
     spGiveOrderToUnit(id, CMD_INSERT,{0, CMD_ATTACK, 0, x,y,z}, CMD_OPT_ALT)
 end
+
 local function RemoveAnyAttack(id)
     spGiveOrderToUnit(id, CMD_REMOVE, CMD_ATTACK, CMD_OPT_ALT)
 end
-Process = function()
+
+local Process = function()
     if not selectedPhoenixes then
         return
     end
@@ -111,25 +107,19 @@ Process = function()
             if not x then
                 return
             end
-            InsertAttackGround(id, x, y , z)
+            InsertAttackGround(id, x, y, z)
         end
     end
 end
 
-function widget:KeyPress(key,m, isRepeat) 
+function widget:KeyPress(key,m, isRepeat) -- note: mods always appear in the order alt,ctrl,shift,meta when iterated  -- we can simulate the same by traversing the table {alt=true,ctrl=true,meta=true,shift=true}
     if isRepeat then
         return
     end
-
     if key == DROP_KEY then
         Process()
     end
-
-
 end
-
-
-
 
 function widget:CommandsChanged() 
     selectedPhoenixes = (WG.selectionDefID or spGetSelectedUnitsSorted() or EMPTY_TABLE)[phoenixDefID]
@@ -165,6 +155,6 @@ function widget:Shutdown()
     end
 end
 
-if f and f.DebugWidget then
+if dev then
     f.DebugWidget(widget)
 end
