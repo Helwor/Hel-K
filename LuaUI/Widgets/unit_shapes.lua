b938bb43e42c276e74c6e9730d450e88fadf06c0
Helwor <hel-k@gmx.fr>
2024-09-09 01:14:01 +0200

    fix depth test, add inner draw option, optimize, bit rewrite

--- LuaUI/Widgets/unit_shapes.lua
+++ LuaUI/Widgets/unit_shapes.lua
@@ -5,8 +5,10 @@ function widget:GetInfo()
 		author    = "Lelousius and aegis, modded Licho, CarRepairer, jK, Shadowfury333",
 		date      = "30.07.2010",
 		license   = "GNU GPL, v2 or later",
-		layer     = -9, -- before draw grid, after outline no shader -- originally layer==2
-		enabled   = true,
+		-- layer     = -9, -- before draw grid, after outline no shader -- originally layer==2
+		-- layer     = 1, -- before draw grid, after outline no shader -- originally layer==2
+		later =  2000,
+		enabled   = false,
 		detailsDefault = 1
 	}
 end
@@ -37,60 +39,60 @@ local math_sin   = math.sin
 local math_abs   = math.abs
 local rad_con    = 180 / math_pi
 
-local GL_KEEP      				= 0x1E00
-local GL_REPLACE   				= 0x1E01
-local GL_ALWAYS 				= GL.ALWAYS
-local GL_NEVER					= GL.NEVER
-local GL_INCR					= GL.INCR
-local GL_ONE_MINUS_DST_ALPHA 	= GL.ONE_MINUS_DST_ALPHA
-local GL_ONE_MINUS_SRC_ALPHA 	= GL.ONE_MINUS_SRC_ALPHA
-local GL_SRC_ALPHA 				= GL.SRC_ALPHA
-local GL_DST_ALPHA 				= GL.DST_ALPHA
-local GL_ONE 					= GL.ONE
-local GL_EQUAL 					= GL.EQUAL
-local GL_ZERO 					= GL.ZERO
-local GL_POLYGON 				= GL.POLYGON
-local GL_GREATER			    = GL.GREATER
-local GL_LEQUAL					= GL.LEQUAL
-local GL_STENCIL_BUFFER_BIT		= GL.STENCIL_BUFFER_BIT
-local GL_FUNC_ADD				= 0x8006
-local GL_FUNC_REVERSE_SUBSTRACT	= 0x800b
-local GL_FUNC_SUBSTRACT			= 0x800a
-local GL_MIN					= 0x8007
-local GL_MAX					= 0x8008
+local GL_KEEP                   = 0x1E00
+local GL_REPLACE                = 0x1E01
+local GL_ALWAYS                 = GL.ALWAYS
+local GL_NEVER                  = GL.NEVER
+local GL_INCR                   = GL.INCR
+local GL_ONE_MINUS_DST_ALPHA    = GL.ONE_MINUS_DST_ALPHA
+local GL_ONE_MINUS_SRC_ALPHA    = GL.ONE_MINUS_SRC_ALPHA
+local GL_SRC_ALPHA              = GL.SRC_ALPHA
+local GL_DST_ALPHA              = GL.DST_ALPHA
+local GL_ONE                    = GL.ONE
+local GL_EQUAL                  = GL.EQUAL
+local GL_ZERO                   = GL.ZERO
+local GL_POLYGON                = GL.POLYGON
+local GL_GREATER                = GL.GREATER
+local GL_LEQUAL                 = GL.LEQUAL
+local GL_STENCIL_BUFFER_BIT     = GL.STENCIL_BUFFER_BIT
+local GL_FUNC_ADD               = 0x8006
+local GL_FUNC_REVERSE_SUBSTRACT = 0x800b
+local GL_FUNC_SUBSTRACT         = 0x800a
+local GL_MIN                    = 0x8007
+local GL_MAX                    = 0x8008
 
-local glClear				= gl.Clear
-local glCreateList 			= gl.CreateList
-local glPushMatrix			= gl.PushMatrix
-local glPopMatrix			= gl.PopMatrix
-local glTranslate			= gl.Translate
-local glScale				= gl.Scale
-local glBeginEnd 			= gl.BeginEnd
-local glVertex 				= gl.Vertex
-local glColorMask 			= gl.ColorMask
-local glBlendFunc 			= gl.BlendFunc
-local glBlendFuncSeparate 	= gl.BlendFuncSeparate
-local glColor 				= gl.Color
-local glDepthMask 			= gl.DepthMask
-local glStencilFunc 		= gl.StencilFunc
-local glStencilOp 			= gl.StencilOp
-local glCallList 			= gl.CallList
-local glBlending 			= gl.Blending
-local glDepthTest 			= gl.DepthTest
-local glDrawListAtUnit 		= gl.DrawListAtUnit
-local glUnit 				= gl.Unit
-local glPolygonOffset 		= gl.PolygonOffset
-local glPushAttrib 			= gl.PushAttrib
-local glStencilTest 		= gl.StencilTest
-local glPopAttrib 			= gl.PopAttrib
-local glDeleteList			= gl.DeleteList
+local glClear               = gl.Clear
+local glCreateList          = gl.CreateList
+local glPushMatrix          = gl.PushMatrix
+local glPopMatrix           = gl.PopMatrix
+local glTranslate           = gl.Translate
+local glScale               = gl.Scale
+local glBeginEnd            = gl.BeginEnd
+local glVertex              = gl.Vertex
+local glColorMask           = gl.ColorMask
+local glBlendFunc           = gl.BlendFunc
+local glBlendFuncSeparate   = gl.BlendFuncSeparate
+local glColor               = gl.Color
+local glDepthMask           = gl.DepthMask
+local glStencilFunc         = gl.StencilFunc
+local glStencilOp           = gl.StencilOp
+local glCallList            = gl.CallList
+local glBlending            = gl.Blending
+local glDepthTest           = gl.DepthTest
+local glDrawListAtUnit      = gl.DrawListAtUnit
+local glUnit                = gl.Unit
+local glPolygonOffset       = gl.PolygonOffset
+local glPushAttrib          = gl.PushAttrib
+local glStencilTest         = gl.StencilTest
+local glPopAttrib           = gl.PopAttrib
+local glDeleteList          = gl.DeleteList
 
-local spIsGUIHidden 		 = Spring.IsGUIHidden
+local spIsGUIHidden          = Spring.IsGUIHidden
 local spGetUnitIsDead        = Spring.GetUnitIsDead
 local spGetUnitHeading       = Spring.GetUnitHeading
 
 local spGetVisibleUnits      = Spring.GetVisibleUnits
-local spIsUnitVisible		 = Spring.IsUnitVisible
+local spIsUnitVisible        = Spring.IsUnitVisible
 local spGetSelectedUnits     = Spring.GetSelectedUnits
 local spGetUnitDefID         = Spring.GetUnitDefID
 local spIsUnitSelected       = Spring.IsUnitSelected
@@ -101,14 +103,14 @@ local spTraceScreenRay     = Spring.TraceScreenRay
 local spGetMouseState      = Spring.GetMouseState
 
 local spGetSpectatingState = Spring.GetSpectatingState
-local spGetUnitTeam		   = Spring.GetUnitTeam
-local spGetMyTeamID		   = Spring.GetMyTeamID
+local spGetUnitTeam        = Spring.GetUnitTeam
+local spGetMyTeamID        = Spring.GetMyTeamID
 local spGetSelectedUnits   = Spring.GetSelectedUnits
 local spGetUnitViewPosition= Spring.GetUnitViewPosition
-local spGetTimer		   = Spring.GetTimer
-local spDiffTimers		   = Spring.DiffTimers
+local spGetTimer           = Spring.GetTimer
+local spDiffTimers         = Spring.DiffTimers
 local spGetHeadingFromVector = Spring.GetHeadingFromVector
-local spGetUnitVelocity	   = Spring.GetUnitVelocity
+local spGetUnitVelocity    = Spring.GetUnitVelocity
 local SafeWGCall = function(fnName, param1) if fnName then return fnName(param1) else return nil end end
 local GetUnitUnderCursor = function(onlySelectable) return SafeWGCall(WG.PreSelection_GetUnitUnderCursor, onlySelectable) end
 local IsSelectionBoxActive = function() return SafeWGCall(WG.PreSelection_IsSelectionBoxActive) end
@@ -134,15 +136,18 @@ local yellow     = {1,1,0.1,maxAlpha}
 local teal       = {0.1,1,1,maxAlpha}
 local red        = {1,0.2,0.1,maxAlpha}
 local hoverColor = teal
+local UPDATE_RATE = 0.03
+local timePassed = 0
 
+local wantHoverInner = true -- color the interior of the shape for hovered unit
 
 local circleDivs      = 32 -- how precise circle? octagon by default
 local innersize       = 0.9 -- circle scale compared to unit radius
-local selectinner     = 1.5
+local midsize     = 1.5
 local outersize       = 1.8 -- outer fade size compared to circle scale (1 = no outer fade)
-local scalefaktor     = 2.8
+local scalefaktor     = 2.9
 local rectangleFactor = 2.7
-local CAlpha          = 0.89
+local CAlpha          = 0.89 -- min alpha
 
 local hoverScaleDuration = 0.05
 local hoverScaleStart    = 0.95
@@ -150,15 +155,16 @@ local hoverScaleEnd      = 1.0
 
 local hoverRestedTime              = 0.05 --Time in ms below which the player is assumed to be rapidly hovering over different units
 local hoverBufferDisplayTime       = 0.05 --Time in ms to keep showing hover when starting box selection
-local hoverBufferScaleSuppressTime = 0.1 --Time in ms to stop box select from doing scale effect on a hovered unit
+local hoverBufferScaleSuppressTime = 0.1 --Time in ms to stop box outermask from doing scale effect on a hovered unit
 
 local boxedScaleDuration = 0.05
 local boxedScaleStart    = 0.9
 local boxedScaleEnd      = 1.0
 
-
 local colorout = {   1,   1,   1,   0 } -- outer color
-local selColor  = {   r,   g,   b,   maxAlpha } -- inner color
+local CAlphaColor = {0, 0, 0, CAlpha} -- border color mask
+
+-- local selColor  = {   r,   g,   b,   maxAlpha } -- inner color
 -- local selColor  = {   r,   g,   b,   minAlpha } -- inner color
 -- local selColor  = {   1,   1,   1,   maxAlpha } -- inner color
 
@@ -187,7 +193,7 @@ local unitStartScaleMap = {}
 local unitEndScaleMap   = {}
 local unitScaleMap      = {}
 local unitDefIDMap      = {}
-local EMPTY_TABLE		= {}
+local EMPTY_TABLE       = {}
 ------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------
 
@@ -383,8 +389,10 @@ local function GetVisibleUnits()
 	if HasVisibilityChanged() then
 		-- local units = spGetVisibleUnits(-1, nil, false)
 		local visibleAllySelUnits, visibleSelected = EMPTY_TABLE, EMPTY_TABLE
+		-- Echo("table.size(visibles) is ", table.size(visibles))
 		if next(visibles) then
-			local hasAllySel = next(allySelUnits) and 	options.allyselectionlevel.value ~= "disabled" and options.allyselectionlevel.value == "enabled"
+			allySelUnits = WG.allySelUnits
+			local hasAllySel = next(allySelUnits) and   options.allyselectionlevel.value ~= "disabled" and options.allyselectionlevel.value == "enabled"
 			if hasAllySel then
 				local overrideAllyTeamID = isSpectating and not options.showallyplayercolours.value and 1
 				visibleAllySelUnits = {}
@@ -429,7 +437,7 @@ local function GetHoveredUnit(dt) --Mostly a convenience function for the animat
 	local unitID = GetUnitUnderCursor(false)
 	local newHoveredUnit = hoveredUnit
 	local cursorIsOn = cursorIsOn
-	if unitID and not selMap[unitID] and Visibles.not_iconsMap[unitID] then
+	if unitID and Visibles.not_iconsMap[unitID] then
 		local unit = Units[unitID]
 		local defID = unit and unit.defID
 		if not newHoveredUnit[1] or unitID ~= newHoveredUnit[1] then
@@ -492,17 +500,18 @@ local function CreateDisplayLists(callbackShape)
 	local displayLists = {}
 
 	local zeroColor = {0, 0, 0, 0}
-	local CAlphaColor = {0, 0, 0, CAlpha}
 
-	displayLists.select = callbackShape.hollow(selColor, outersize, selectinner) 
-	displayLists.hover = callbackShape.hollow(teal, outersize, selectinner)
+	displayLists.outermask = callbackShape.hollow(CAlphaColor, outersize, midsize) 
+	displayLists.hover_inner_mask = callbackShape.hollow(CAlphaColor, innersize, midsize) 
+	displayLists.hover = callbackShape.hollow(teal, outersize, midsize)
 
-	-- displayLists.invertedSelect = callbackShape.hollow(colorout, selColor, outersize, selectinner)
-	-- displayLists.inner = callbackShape.plain(zeroColor, innersize)
-	displayLists.innermask = callbackShape.plain(nil, selectinner)
-	displayLists.outermask = callbackShape.hollow(nil,outersize, selectinner)
+	-- displayLists.invertedSelect = callbackShape.hollow(colorout, selColor, outersize, midsize)
+	displayLists.inner = callbackShape.hollow(nil,innersize, midsize)
+
+	displayLists.inner_plain_mask = callbackShape.plain(nil, midsize)
+	displayLists.outer = callbackShape.hollow(nil,outersize, midsize)
 	-- displayLists.kill = callbackShape.plain(nil, outersize)
-	-- displayLists.shape = callbackShape.hollow(zeroColor, CAlphaColor, innersize, selectinner)
+	-- displayLists.shape = callbackShape.hollow(zeroColor, CAlphaColor, innersize, midsize)
 	
 	return displayLists
 end
@@ -644,11 +653,15 @@ local function CreateTriangleLists()
 end
 
 local function DestroyShape(shape)
-	glDeleteList(shape.select)
-	glDeleteList(shape.hover)
+	for k,list in pairs(shape) do
+		glDeleteList(list)
+	end
+
+	-- glDeleteList(shape.outermask)
+	-- glDeleteList(shape.hover)
 	-- glDeleteList(shape.invertedSelect)
 	-- glDeleteList(shape.inner)
-	glDeleteList(shape.innermask)
+	-- glDeleteList(shape.inner_plain_mask)
 	-- glDeleteList(shape.kill)
 	-- glDeleteList(shape.shape)
 end
@@ -735,7 +748,7 @@ local HEADING_TO_RAD = 1/32768*math.pi
 local RADIANS_PER_COBANGLE = math.pi / 32768
 
 local function UpdateUnitListScale(unitList)
-	if not unitList[1] then
+	if not (unitList and unitList[1]) then
 		return
 	end
 	local now = spGetTimer()
@@ -784,8 +797,6 @@ local function UpdateUnitListRotation(unitList)
 		degrot[unitID] = rot
 	end
 end
-local UPDATE_RATE = 0.033
-local timePassed = 0
 local function HasToDraw()
 	if not (visibleSelected[1] or hoveredUnit[1] or visibleBoxed[1]) and not hasVisibleAllySelections then
 		return
@@ -798,7 +809,7 @@ end
 function widget:Update(dt)
 	timePassed = timePassed + dt
 	
-	if timePassed < UPDATE_RATE and not HasToDraw() then
+	if timePassed < UPDATE_RATE then
 		return
 	end
 	timePassed = 0
@@ -806,6 +817,10 @@ function widget:Update(dt)
 		hoveredUnit, cursorIsOn = GetHoveredUnit(dt)
 	end
 	visibleAllySelUnits, visibleSelected, visibleBoxed = GetVisibleUnits()
+
+	if not HasToDraw() then
+		return
+	end
 
 	if visibleBoxed[1] then
 		cursorIsOn = "self"
@@ -837,36 +852,7 @@ local function DrawUnitShapes(unitList, color, stencil)
 	if not unitList[1] then
 		return
 	end
-	-- innermask
-	if stencil then
-		glStencilTest(true)
-		glStencilFunc(GL_NEVER, 0, 1)
-		glStencilOp(GL_INCR, GL_KEEP, GL_KEEP)
-
-		for i = 1, #unitList do
-			local unitID = unitList[i]
-			local udid = unitDefIDMap[unitID] or spGetUnitDefID(unitID)
-			local unit = unitConf[udid]
-			local scale = unitScaleMap[unitID] or 1
-
-			if unit then
-				-- glColor(color)
-				-- local x,y,z = spGetUnitViewPosition(unitID)
-				-- glPushMatrix()
-				-- glTranslate(x,y,z)
-				-- glScale(unit.xscale * scale, 1.0, unit.zscale * scale)
-				-- glCallList(unit.shape.innermask)
-				-- glPopMatrix()
-				glDrawListAtUnit(unitID, unit.shape.innermask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
-			end
-		end
-		-- glDepthMask(false)
-		-- glColorMask(true, true,true,true)
-		-- glColor(color)
-	    glStencilFunc(GL_EQUAL, 0, 0xff);
-	    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
-	end
-	-----
+	local len = #unitList
 	glBlending(true)
 	glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA)
 	glColorMask(false,false,false,true)
@@ -877,7 +863,7 @@ local function DrawUnitShapes(unitList, color, stencil)
 		local scale = unitScaleMap[unitID] or 1
 
 		if unit then
-			glDrawListAtUnit(unitID, unit.shape.select, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+			glDrawListAtUnit(unitID, unit.shape.outermask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
 		end
 	end
 	glColor(color)
@@ -892,7 +878,93 @@ local function DrawUnitShapes(unitList, color, stencil)
 		local scale = unitScaleMap[unitID] or 1
 
 		if unit then
-			glDrawListAtUnit(unitID, unit.shape.outermask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+			glDrawListAtUnit(unitID, unit.shape.outer, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+		end
+	end
+end
+
+local function DrawUnitShapes(unitList, color, stencil, hover_inner, exception)
+	if not unitList[1] then
+		return
+	end
+	local len = #unitList
+	-- inner_plain_mask
+	if stencil then -- used for any group except the hovered
+		glStencilTest(true)
+		glStencilFunc(GL_NEVER, 0, 1)
+		glStencilOp(GL_INCR, GL_KEEP, GL_KEEP)
+
+		for i = 1, len do
+			local unitID = unitList[i]
+			-- if not done then
+			if not exception or not exception[unitID] then
+				local udid = unitDefIDMap[unitID] or spGetUnitDefID(unitID)
+				local unit = unitConf[udid]
+				local scale = unitScaleMap[unitID] or 1
+
+				if unit then
+					-- glColor(color)
+					-- local x,y,z = spGetUnitViewPosition(unitID)
+					-- glPushMatrix()
+					-- glTranslate(x,y,z)
+					-- glScale(unit.xscale * scale, 1.0, unit.zscale * scale)
+					-- glCallList(unit.shape.inner_plain_mask)
+					-- glPopMatrix()
+					glDrawListAtUnit(unitID, unit.shape.inner_plain_mask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+				end
+			end
+		end
+		-- glDepthMask(false)
+		-- glColorMask(true, true,true,true)
+		-- glColor(color)
+		glStencilFunc(GL_EQUAL, 0, 0xff);
+		glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+	end
+	-----
+	glBlending(true)
+
+	glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA)
+	glBlendFuncSeparate(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA)
+	glColorMask(false,false,false,true)
+	for i = 1, len do
+		local unitID = unitList[i]
+		if not exception or not exception[unitID] then
+			local udid = unitDefIDMap[unitID] or spGetUnitDefID(unitID)
+			local unit = unitConf[udid]
+			local scale = unitScaleMap[unitID] or 1
+
+			if unit then
+				if hover_inner then
+					glDrawListAtUnit(unitID,unit.shape.hover_inner_mask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+				else
+					glDrawListAtUnit(unitID,unit.shape.outermask, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+				end
+				
+
+			end
+		end
+	end
+
+	glColor(color)
+	glColorMask(true,true,true,true)
+	glBlending(true)
+	glBlendFuncSeparate(GL_ONE_MINUS_DST_ALPHA, GL_DST_ALPHA, GL_ONE, GL_ONE)
+
+	for i = 1, len do
+		local unitID = unitList[i]
+		if not exception or not exception[unitID] then
+			local udid = unitDefIDMap[unitID] or spGetUnitDefID(unitID)
+			local unit = unitConf[udid]
+			local scale = unitScaleMap[unitID] or 1
+
+			if unit then
+				if hover_inner then
+					glDrawListAtUnit(unitID, unit.shape.inner, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+				else
+					glDrawListAtUnit(unitID, unit.shape.outer, false, unit.xscale * scale, 1.0, unit.zscale * scale, degrot[unitID], 0, degrot[unitID], 0)
+				end
+				
+			end
 		end
 	end
 
@@ -904,8 +976,11 @@ local function DrawUnitShapes(unitList, color, stencil)
 end
 local function DrawShapes()
 	hoverColor = cursorIsOn == "enemy" and red or (cursorIsOn == "ally" and yellow or teal)
-
-	DrawUnitShapes(visibleSelected, rgba, 'select', true)
+	-- local exception = {}
+	-- for i,id in ipairs(visibleBoxed) do
+	--  exception[id] = true
+	-- end
+	DrawUnitShapes(visibleSelected, rgba, true)
 	if hasVisibleAllySelections then
 		if isSpectating and options.showallyplayercolours.value then
 			if fullSelect then hoverCall = "hover" end
@@ -914,7 +989,6 @@ local function DrawShapes()
 			for i = 1, #teams do
 				local team = teams[i]
 				if visibleAllySelUnits[team+1] then
-					
 				  DrawUnitShapes(visibleAllySelUnits[team+1], teamColors[team], true)
 				end
 			end
@@ -922,12 +996,14 @@ local function DrawShapes()
 			DrawUnitShapes(visibleAllySelUnits[1], yellow, true)
 		end
 	end
-	DrawUnitShapes(hoveredUnit, hoverColor, false)
-	DrawUnitShapes(visibleBoxed, hoverColor, true)
 	-- glClear(GL_STENCIL_BUFFER_BIT)
+	DrawUnitShapes(visibleBoxed, hoverColor, true)
+	DrawUnitShapes(hoveredUnit, hoverColor, false, wantHoverInner)
+	-- gl.StencilTest(false)
+	glClear(GL_STENCIL_BUFFER_BIT)
 end
-
-function widget:DrawWorldPreUnit()
+local function Draw()
+	-- glClear(GL_STENCIL_BUFFER_BIT)
 	if timePassed == 0 then
 		if globalDraw then
 			glDeleteList(globalDraw)
@@ -941,20 +1017,33 @@ function widget:DrawWorldPreUnit()
 	if not globalDraw then
 		return
 	end
-	glDepthTest(GL_GREATER)
-	glCallList(globalDraw)
-end
-function widget:DrawWorld()
-	if not globalDraw then
-		return
-	end
-	glDepthTest(GL_LEQUAL)
-	glCallList(globalDraw)
-	glDepthTest(false)
-	glBlending('reset')
-	glColor(1,1,1,1)
 
+	-- glDepthTest(GL.ALWAYS)
+	-- glCallList(globalDraw)
+	-- glDepthTest(GL_LEQUAL)
+	-- glBlending('reset')
+	glDepthTest(GL.ALWAYS)
+	glCallList(globalDraw)
+	glDepthTest(GL_LEQUAL)
+	glBlending('reset')
 end
+local function Init()
+	glDepthTest(GL_LEQUAL)-- FIXME somewhere the depth test is not right when the widget is loaded for the first time
+	widget.DrawWorldPreUnit = Draw
+end
+
+widget.DrawWorldPreUnit = Init
+-- function widget:DrawWorld()
+--  if not globalDraw then
+--      return
+--  end
+--  glDepthTest(GL_LEQUAL)
+--  glCallList(globalDraw)
+--  glDepthTest(false)
+--  glBlending('reset')
+--  glColor(1,1,1,1)
+-- end
+
 function widget:MousePress(_,_,button)
 	if button == 1 then
 		checkForSelBox = true
